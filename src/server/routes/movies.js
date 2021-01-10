8588f5a346a9cd23296d8a4d3921aa67793bee99
Yasser Batas <yassk8@gmail.com>
2021-01-09 23:02:11 -0600

    Add delete movie route

--- src/server/routes/movies.js
+++ src/server/routes/movies.js
@@ -11,6 +11,8 @@ router.get('/:id', async ctx => moviesController.getMovieById(ctx))
 
 router.post('/', async ctx => moviesController.createMovie(ctx))
 
+router.delete('/:id', async ctx => moviesController.deleteMovie(ctx))
+
 router.get('/upcoming', async ctx => {})
 
 module.exports = router
