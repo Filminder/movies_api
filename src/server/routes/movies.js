0557ad5d6238c1fe29c915388f0f2823ee54ffd2
Yasser Batas <yassk8@gmail.com>
2021-01-09 19:48:36 -0600

    Refactor movies route file

--- src/server/routes/movies.js
+++ src/server/routes/movies.js
@@ -1,51 +1,15 @@
 const Router = require('koa-router')
-const queries = require('../db/queries/movies')
-const successResponse = require('../../views/success')
-const responseError = require('../../views/error')
+const moviesController = require('../../controllers/movies')
 
 const router = new Router({
   prefix: '/api/v1/movies'
 })
 
-router.get('/', async ctx => {
-  try {
-    const movies = await queries.getAllMovies()
-    return successResponse(ctx, movies)
-  } catch (error) {
-    return responseError(ctx, 404, 'Something went wrong')
-  }
-})
+router.get('/', async ctx => moviesController.getAllMovies(ctx))
 
-router.get('/:id', async ctx => {
-  try {
-    const movie = await queries.getSingleMovie(ctx.params.id)
+router.get('/:id', async ctx => moviesController.getMovieById(ctx))
 
-    if (!movie.length) {
-      return responseError(ctx, 404, 'That movie does not exist')
-    }
-
-    return successResponse(ctx, movie)
-  } catch (error) {
-    return responseError(ctx, 404, 'That movie does not exist')
-  }
-})
-
-router.post('/', async ctx => {
-  try {
-    const movie = await queries.addMovie(ctx.request.body)
-    if (movie.length) {
-      successResponse(ctx, movie)
-    } else {
-      responseError(ctx, 400, 'Something went wrong.')
-    }
-  } catch (err) {
-    responseError(
-      ctx,
-      400,
-      'Something went wrong. Maybe this movie already exist'
-    )
-  }
-})
+router.post('/', async ctx => moviesController.createMovie(ctx))
 
 router.get('/upcoming', async ctx => {})
 
